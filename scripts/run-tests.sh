e20101b452cbe453bb3256cada37891d9f860298
Jason Ozias <jason.ozias@gmail.com>
2018-10-05 08:33:27 -0400

    different script based on rust channel

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -ev
 echo ${TRAVIS_RUST_VERSION}
-cargo build --features serde
-cargo test --features serde
-cargo doc --features serde
+
+if [ "${TRAVIS_RUST_VERSION}" = "stable" ]; then
+    cargo build --features serde
+    cargo test --features serde
+    cargo doc --features serde
+elif [ "${TRAVIS_RUST_VERSION}" = "beta" ]; then
+    cargo build --features serde
+    cargo test --features serde
+    cargo doc --features serde
+else
+    cargo build --features serde,unstable
+    cargo test --features serde,unstable
+    cargo doc --features serde,unstable
+fi
