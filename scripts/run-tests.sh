11898e5788d7d1e80d6a5bfa48b85fa0dbbc1007
Jason Ozias <jason.ozias@gmail.com>
2018-10-06 10:18:04 -0400

    Removed doc step from travis build, docs.rs takes care of this

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,13 +4,10 @@ set -ev
 if [ "${TRAVIS_RUST_VERSION}" = "stable" ]; then
     cargo build --features serde
     cargo test --features serde
-    cargo doc --features serde
 elif [ "${TRAVIS_RUST_VERSION}" = "beta" ]; then
     cargo build --features serde
     cargo test --features serde
-    cargo doc --features serde
 else
     cargo build --features serde,unstable
     cargo test --features serde,unstable
-    cargo doc --features serde,unstable
 fi
